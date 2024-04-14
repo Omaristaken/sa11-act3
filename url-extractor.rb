4091d9be14bc70b12295ebc2240f69c2757275f4
Omar <bomarm567@gmail.com>
2024-04-14 11:10:11 -0500

    revised

--- url-extractor.rb
+++ url-extractor.rb
@@ -4,7 +4,7 @@ def extract_urls(text)
   urls.each { |url| puts url }
 end
 
-# Example usage:
+
 sample_text = "Visit our site at http://www.example.org for more information. Check out our search page at
 https://example.com/search?q=ruby+regex. Don't forget to ftp our resources at ftp://example.com/resources."
 
